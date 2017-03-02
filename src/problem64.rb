197a524f3c3912dd1db7c336af31727f1a37ea7b
Ilan Goldenstein <angeli106@gmail.com>
2017-03-02 18:10:57 +0200

    started with 64
    Signed-off-by: Ilan Goldenstein <angeli106@gmail.com>

--- src/problem64.rb
+++ src/problem64.rb
@@ -4,4 +4,10 @@ def nearestWholeRoot(n)
 	Math.sqrt n.downto(1).find {|n| Math.sqrt(n) % 1 == 0}
 end
 
+def next(free, nom, n, denom)
+	result = {}
+	result['nom'] = n - (denom ** 2)
+	result['denom'] = result['nom'] - denom
+	result['free'] = 
+
 puts nearestWholeRoot(27)
